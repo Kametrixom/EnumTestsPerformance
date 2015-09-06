70c6ca5327764209bb7f226b853f29af4aace370
Kametrixom <kametrixom@icloud.com>
2015-09-06 11:12:24 +0200

    Initial Commit

---
+++ EnumTests/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EnumTests
+//
+//  Created by Silvan Mosberger on 06/09/15.
+//  Copyright © 2015 Kametrixom Tikara. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
